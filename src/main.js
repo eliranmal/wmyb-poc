1084b830da496b4dd903f9a143c1e410db7598f8
eliranm <eliranm@liveperson.com>
2017-06-04 01:12:29 +0300

    webpack, scaffolding

---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+require('../styles/reset.css');
+
+require('pixi');
+require('p2');
+require('phaser');
+
+var Phaser = require('phaser');
+
+var game = new Phaser.Game('100', '100', Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const phaserModulePath = path.join(__dirname, '/node_modules/phaser/');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: '[name].bundle.js',
+    },
+    devtool: 'cheap-eval-source-map',
+    module: {
+        rules: [
+            // styles
+            {
+                test: /styles\/.*\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            // phaser dependencies
+            {
+                test: /pixi\.js$/,
+                use: 'expose-loader?PIXI'
+            },
+            {
+                test: /phaser-split\.js$/, 
+                use: 'expose-loader?Phaser'
+            },
+            {
+                test: /p2\.js$/, 
+                use: 'expose-loader?p2'
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            'phaser': path.join(phaserModulePath, 'build/custom/phaser-split.js'),
+            'pixi': path.join(phaserModulePath, 'build/custom/pixi.js'),
+            'p2': path.join(phaserModulePath, 'build/custom/p2.js'),
+        }
+    },
+};
